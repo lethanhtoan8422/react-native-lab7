77a435e21c4022d8f8b5fe40ec4190ddf42575ba
Your Name <you@example.com>
2023-11-07 14:51:16 +0700

    finish

--- src/shop/Shop.js
+++ src/shop/Shop.js
@@ -5,44 +5,46 @@ import { Ionicons } from '@expo/vector-icons'
 import { TouchableOpacity } from 'react-native-web'
 
 const Shop = ({ navigation }) => {
-    const [shops, setShops] = useState([
-        {
-         "path": "https://res.cloudinary.com/dsvx8aorm/image/upload/v1698736026/Image_y1zlsk.png",
-         "name": "Kitanda Espresso & Acai-U District",
-         "status": true,
-         "deliverytime": "5-10 minutes",
-         "address": "1121 NE 45 St",
-         "id": "1"
-        },
-        {
-         "path": "https://res.cloudinary.com/dsvx8aorm/image/upload/v1698736027/Image1_s6pc4n.png",
-         "name": "Jewel Box Cafe",
-         "status": false,
-         "deliverytime": "10-15 minutes",
-         "address": "1145 GE 54 St",
-         "id": "2"
-        },
-        {
-         "path": "https://res.cloudinary.com/dsvx8aorm/image/upload/v1698736027/Image2_ddgnql.png",
-         "name": "Javasti Cafe",
-         "status": false,
-         "deliverytime": "15-20 minutes",
-         "address": "1167 GE 54 St",
-         "id": "3"
-        },
-        {
-         "path": "https://res.cloudinary.com/dsvx8aorm/image/upload/v1698736028/Image3_zyoog5.png",
-         "name": "Nguyen Lengend",
-         "status": false,
-         "deliverytime": "15-20 minutes",
-         "address": "HCM city",
-         "id": "4"
-        }
-       ])
+    const [shops, setShops] = useState(
+        [
+            {
+            "path": "https://res.cloudinary.com/dsvx8aorm/image/upload/v1698736026/Image_y1zlsk.png",
+            "name": "Kitanda Espresso & Acai-U District",
+            "status": true,
+            "deliverytime": "5-10 minutes",
+            "address": "1121 NE 45 St",
+            "id": "1"
+            },
+            {
+            "path": "https://res.cloudinary.com/dsvx8aorm/image/upload/v1698736027/Image1_s6pc4n.png",
+            "name": "Jewel Box Cafe",
+            "status": false,
+            "deliverytime": "10-15 minutes",
+            "address": "1145 GE 54 St",
+            "id": "2"
+            },
+            {
+            "path": "https://res.cloudinary.com/dsvx8aorm/image/upload/v1698736027/Image2_ddgnql.png",
+            "name": "Javasti Cafe",
+            "status": false,
+            "deliverytime": "15-20 minutes",
+            "address": "1167 GE 54 St",
+            "id": "3"
+            },
+            {
+            "path": "https://res.cloudinary.com/dsvx8aorm/image/upload/v1698736028/Image3_zyoog5.png",
+            "name": "Nguyen Lengend",
+            "status": false,
+            "deliverytime": "15-20 minutes",
+            "address": "HCM city",
+            "id": "4"
+            }
+        ]
+    )
 
     useEffect(() => {
         let apiGetDataShops = async () => {
-            let data = await axios.get("https://654097b845bedb25bfc2225b.mockapi.io/api/lab7/shop")
+            let data = await axios.get("https://6549d7a4e182221f8d51f7c3.mockapi.io/api/lap07/shop")
             setShops(data.data);
         }
         apiGetDataShops()
@@ -62,38 +64,40 @@ const Shop = ({ navigation }) => {
             <ScrollView style={{width : '100%', height : '90%'}}>
                 {
                     shops.map((shop, index) => (
-                        <View style={styles.viewShop} key={index}>
-                            <Image source={{uri : shop.path}} style={{width : '100%', height : '100px', borderRadius : '10px'}}/>
-                            <View style={styles.viewStatus}>
-                                <View style={styles.flexRowBetween}>
-                                    {
-                                        shop.status
-                                        ? <Ionicons name='checkmark-outline' size={25} style={{width : '20%', color : 'green'}}/>
-                                        : <Ionicons name='lock-closed-outline' size={25} style={{width : '20%', color : 'red'}}/>
-                                    }
-                                    <Text 
-                                    style={{
-                                        width : '80%',fontSize : '15px',
-                                        color : shop.status ? "green" : "red"
-                                    }}>
-                                        {shop.status ? "Accepting Orders" : "Tempory Unavailable"}
-                                    </Text>
+                        <TouchableOpacity onPress={() => navigation.navigate("Drink", {shopID : shop.id})}>
+                            <View style={styles.viewShop} key={index}>
+                                <Image source={{uri : shop.path}} style={{width : '100%', height : '100px', borderRadius : '10px'}}/>
+                                <View style={styles.viewStatus}>
+                                    <View style={styles.flexRowBetween}>
+                                        {
+                                            shop.status
+                                            ? <Ionicons name='checkmark-outline' size={25} style={{width : '20%', color : 'green'}}/>
+                                            : <Ionicons name='lock-closed-outline' size={25} style={{width : '20%', color : 'red'}}/>
+                                        }
+                                        <Text 
+                                        style={{
+                                            width : '80%',fontSize : '15px',
+                                            color : shop.status ? "green" : "red"
+                                        }}>
+                                            {shop.status ? "Accepting Orders" : "Tempory Unavailable"}
+                                        </Text>
+                                    </View>
+                                    <View style={styles.flexRowBetweenMinutes}>
+                                        <Ionicons name='time-outline' size={25} style={{width : '20%', color : 'green'}}/>
+                                        <Text style={{width : '100%',fontSize : '15px', color : 'red'}}>{shop.deliverytime}</Text>
+                                    </View>
+                                    <Ionicons name='map-outline' size={25} style={{width : '10%', color : 'green'}}/>
                                 </View>
-                                <View style={styles.flexRowBetweenMinutes}>
-                                    <Ionicons name='time-outline' size={25} style={{width : '20%', color : 'green'}}/>
-                                    <Text style={{width : '100%',fontSize : '15px', color : 'red'}}>{shop.deliverytime}</Text>
-                                </View>
-                                <Ionicons name='map-outline' size={25} style={{width : '10%', color : 'green'}}/>
+                                <Text 
+                                style={{fontSize : '18px', fontWeight : 'bold', marginLeft : '10px'}}>
+                                    {shop.name}
+                                </Text>
+                                <Text 
+                                style={{fontSize : '15px', fontWeight : 'bold', color : "rgba(188,193,202,1)", marginLeft : '10px'}}>
+                                    {shop.address}
+                                </Text>
                             </View>
-                            <Text 
-                            style={{fontSize : '18px', fontWeight : 'bold', marginLeft : '10px'}}>
-                                {shop.name}
-                            </Text>
-                            <Text 
-                            style={{fontSize : '15px', fontWeight : 'bold', color : "rgba(188,193,202,1)", marginLeft : '10px'}}>
-                                {shop.address}
-                            </Text>
-                        </View>
+                        </TouchableOpacity>
                     ))
                 }
             </ScrollView>
@@ -113,7 +117,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingHorizontal: '20px',
         backgroundColor : 'rgba(188,193,202,0.3)',
-
     },
     flexRowBetween : {
         width : '50%',
@@ -139,10 +142,8 @@ const styles = StyleSheet.create({
         height: '10%',
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'flex-start',
+        justifyContent: 'space-between',
         alignItems: 'center',
-        borderWidth: '1px',
-        borderColor: 'black'
     },
     btnBack: {
         height: '100%',
